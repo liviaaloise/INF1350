fd307d0b9082a1ff02c6ccb0b581a866a403a911
costalonga <33266172+costalonga@users.noreply.github.com>
2019-04-09 05:00:52 -0300

    Add files via upload
    Versão final do projeto, alarme com modos de horário (com e sem alarme ligado), hora do alarme, ajuste de horas e ajuste de minutos para relógio e para alarme. 
    Alarme apita durante 1minuto a emitindo um som à uma frequência (próximo a frquência de uma nota musical ré), seguido de mais apitos a uma frequência de 342Hz (próximo à uma nota musical fá).
    No módulo "feature_alarm_with_music" são tocadas variações dessas notas tentando reproduzir uma música conhecida, porém vimos que a campainha não reagia bem a frequências acima de 349Hz.
    Todos os outros requisitos básicos do projeto foram satisfeitos.
    O nosso tratamento de debounce, nos permite segurar um botão por tempo indeterminado, e só será contado uma única como apertado, somente ao soltar o botão. 
    Para utilizar a função de pressionar o botão 3+1 para voltar ao primeiro estado, deve-se pressionar primeiro o botão 3 logo em seguida o botão 1, pois ao pressionar o botão 3 por último, a sua função de ir para o próximo estado ocorrerá antes.

--- mini1/main.ino
+++ mini1/main.ino
@@ -15,7 +15,6 @@
 #define lah 440
 #define sih 493
 
-//int music[15] = {mih, mih, fah, sol, sol, fah, mih, reh, doh, doh, reh, mih, mih, reh, reh};
 int music[30] = {mih, mih, fah, sol, sol, fah, mih, reh, doh, doh, reh, mih, mih, reh, reh,
                  mih, mih, fah, sol, sol, fah, mih, reh, doh, doh, reh, mih, reh, doh, doh};
 int music_note = 0;
@@ -40,30 +39,32 @@ unsigned long timerDuration;
 unsigned long buzz_ts; //buzzer timer start
 unsigned long buzz_td; //buzzer timer duration
 bool turn_on_buzzer = false;
+int led_state = LOW;
 
 void timer_set(int ms) {
   timerStart = millis();
   timerDuration = (long)(unsigned)ms;
 }
-// ------------------------------ TODO: DEBOUNCE HANDLER
+
+// ------------------------------ DEBOUNCE HANDLER VARS
 int butStates[3] = {HIGH, HIGH, HIGH};
 int currentStates[3];
-unsigned long buttonTime[3] = {0, 0, 0};
-unsigned long debounceTime;
 int buttons[3] = {KEY1, KEY2, KEY3};
 int butLastStates[3] = {HIGH, HIGH, HIGH};
-// ------ TODO: DEBOUNCE HANDLER ----- Button 3
-int but3_CurrState = HIGH; //current state
-int but3_LastState = LOW;
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-// ------------------------------ TODO: DEBOUNCE HANDLER
-
+unsigned long buttonTime[3] = {0, 0, 0};
+unsigned long debounceTime;
+unsigned long timer_b1_b3 = 950;  // time limit between but3 press and but1 press to go back to first state
+unsigned long debounceDelay = 50;    // the debounce time = 50ms
+unsigned long lastDebounceTime = 0;
+unsigned long but1_time_pressed = 0;
+unsigned long but3_time_pressed = 0;
+// ------------------------------ DEBOUNCE HANDLER VARS
 
 int modes[7] = { CLOCK, ALARM_ON , ALARM, SET_CLOCK_H, SET_CLOCK_M, SET_ALARM_H, SET_ALARM_M };
 int modeIndex = 21;
 int lastMode = 28; //initial value = 28 (invalid mode)
 int counter = 0; //count how many seconds the program has been in the same state
+int count_minutes = 0; //count every 60seconds
 unsigned long last_pressed_time = 0;
 int aux = 0;
 int current_mode = 0;
@@ -129,26 +130,17 @@ void setup() {
   pinMode(KEY3, INPUT_PULLUP);
   buzz_td = sizeof(music)*1000;
   buzz_ts = 0;
-  int tempo = 1000; // TODO: multiplicar por 60 para fazer relogio contar 24 horas ao inves de 24min
+  int tempo = 1000;
   timer_set(tempo);
 }
 // END OF SETUP
 
 /* Main program */
 void loop() {
-
-  //todo delete comment
-  // Serial.print(lastMode);
-  // Serial.print("\t:\t");
-  // Serial.print(modeIndex);
-  // Serial.print("\n");
-
   digitalWrite(BUZZ, HIGH);
-  //TODO: Fix debounce for all keys using an array
   int but1 = digitalRead(KEY1);
   int but2 = digitalRead(KEY2);
   int but3 = digitalRead(KEY3);
-  // int read_buttons[3] = {but1, but2, but3}; //TODO: Fix debounce for all buttons
 
   if((timeNow - buzz_ts >= buzz_td)) {
     turn_on_buzzer = false;
@@ -156,6 +148,7 @@ void loop() {
      music_note = 0;
   }
 
+  //  Debounce Handler
   for (int i = 0; i < 3; i++) {
     butStates[i] = digitalRead(buttons[i]);
     if ((butStates[i] == LOW) && (butLastStates[i] == HIGH)) {
@@ -169,6 +162,8 @@ void loop() {
     if (butStates[0] != currentStates[0]) {
       currentStates[0] = butStates[0];
       if (currentStates[0] == HIGH) {
+        but1_time_pressed = millis();
+        counter = 0;
         Serial.println("PRESSED BUT ONE!");
       }
     }
@@ -178,41 +173,73 @@ void loop() {
     if (butStates[1] != currentStates[1]) {
       currentStates[1] = butStates[1];
       if (currentStates[1] == HIGH) {
+        counter = 0;
+        if(modeIndex==SET_CLOCK_H) {
+          leds_off();
+          update_clk_hour();
+        }
+        else if(modeIndex==SET_CLOCK_M) {
+          leds_off();
+          update_clk_min();
+        }
+        else if(modeIndex==SET_ALARM_H) {
+          leds_off();
+          update_alm_hour();
+        }
+        else if(modeIndex==SET_ALARM_M) {
+          leds_off();
+          update_alm_min();
+        }
         Serial.println("\t\t\tPRESSED BUT TWO!");
       }
     }
   }
   //BUT3
-  if ((timeNow - buttonTime[2]) > debounceDelay) {
+  if ((buttonTime[2] != 0)&&((timeNow - buttonTime[2]) > debounceDelay)) {
     if (butStates[2] != currentStates[2]) {
       currentStates[2] = butStates[2];
       if (currentStates[2] == HIGH) {
+        but3_time_pressed = millis();
         Serial.println("\t\t\t\t\t\tPRESSED BUT THREE!");
+        counter = 0;
         lastMode = modeIndex; //save last mode before going to next mode
         goto_next_mode();
       }
     }
   }
+  //BUT1+3
+  if ((but1_time_pressed!=0) && (but3_time_pressed!=0)) {
+    if (abs(but1_time_pressed - but3_time_pressed) <= timer_b1_b3) {
+      leds_off();
+      modeIndex = 21; //reset mode to first state
+    }
+  }
   butLastStates[0] = but1;
   butLastStates[1] = but2;
   butLastStates[2] = but3;
+  // end of Debounce
 
   timeNow = millis();
-  if(timeNow - timerStart >= timerDuration) {
-    update_time();
+  if(timeNow - timerStart > timerDuration) {
+    count_minutes += 1;
+    if (count_minutes >= 60) {
+      count_minutes = 0;
+      update_time();
+    }
     int tempo = 1000;
     timer_set(tempo);
 
     // Se 10seg se passarem e nada for pressionado estado volta para inicio
     if(modeIndex==SET_CLOCK_H || modeIndex==SET_CLOCK_M || modeIndex==SET_ALARM_H || modeIndex==SET_ALARM_M) {
+      led_state = !led_state;
       counter += 1;
       if (counter > 10) {
         counter = 0;
+        leds_off();
         modeIndex = CLOCK;
       }
     }
 
-    // TODO: adicionar ifs para ver horas quando fizer versao final do relogio contando 24horas ao inves de 24min
     if (modeIndex == ALARM_ON || modeIndex == ALARM) {
       if (turn_on_buzzer == true){
           // digitalWrite(BUZZ, HIGH);
@@ -220,16 +247,18 @@ void loop() {
           //play_alarm_song();
           tone(BUZZ, 291, 500);
       }
-      if (clk_h0 == alm_h0) {
-        if (clk_m1 == alm_m1) {
-          if (clk_m0 == alm_m0) {
-            if (turn_on_buzzer == false) {
-              buzz_ts = millis();
-              turn_on_buzzer = true;
-              music_note = 0;
-              // TODO: Change buzzer ring;
-              //play_alarm_song();
-              tone(BUZZ, 342, 500);
+      if (clk_h1 == alm_h1) {
+        if (clk_h0 == alm_h0) {
+          if (clk_m1 == alm_m1) {
+            if (clk_m0 == alm_m0) {
+              if (turn_on_buzzer == false) {
+                buzz_ts = millis();
+                turn_on_buzzer = true;
+                music_note = 0;
+                // TODO: Change buzzer ring;
+                //play_alarm_song();
+                tone(BUZZ, 342, 500);
+              }
             }
           }
         }
@@ -237,66 +266,43 @@ void loop() {
     }
     else {
       turn_on_buzzer = false;
-      // digitalWrite(BUZZ, HIGH);
-      // music_note = 0;
     }
     digitalWrite(BUZZ,HIGH);
-  }
-
-  if(!but1) {
-    // last_pressed_time = millis();
-    counter = 0;
-  }
-  if(!but2) {
-    // last_pressed_time = millis();
-    counter = 0;
   }
 
   switch(modeIndex){
     case CLOCK: //mostrar horario com alarme desligado
       digitalWrite(LED1, LOW);
       show_time(clk_h1, clk_h0, clk_m1, clk_m0);
-      Serial.println(modeIndex); //delete
       break;
     case ALARM_ON: // mostrar horario com alarme ligado
       digitalWrite(LED2, LOW);
       show_time(clk_h1, clk_h0, clk_m1, clk_m0);
-      Serial.println(modeIndex); //delete
       break;
     case ALARM: //mostrar horario do alarme
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
-//      show_time(alarm_time);
       show_time(alm_h1, alm_h0, alm_m1, alm_m0);
-      Serial.println(modeIndex); //delete
       break;
     case SET_CLOCK_H:
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED3,LOW);
-      show_stateN(3);
-//      set_time()
-      Serial.println(modeIndex); //delete
+      digitalWrite(LED1, led_state);
+      digitalWrite(LED3, led_state);
+      show_time(clk_h1, clk_h0, clk_m1, clk_m0);
       break;
     case SET_CLOCK_M:
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED4, LOW);
-      show_stateN(4);
-//      set_time();
-      Serial.println(modeIndex); //delete
+      digitalWrite(LED1, led_state);
+      digitalWrite(LED4, led_state);
+      show_time(clk_h1, clk_h0, clk_m1, clk_m0);
       break;
     case SET_ALARM_H:
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED3,LOW);
-      show_stateN(5);
-//      set_time()
-      Serial.println(modeIndex); //delete
+      digitalWrite(LED2, led_state);
+      digitalWrite(LED3, led_state);
+      show_time(alm_h1, alm_h0, alm_m1, alm_m0);
       break;
     case SET_ALARM_M:
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED4, LOW);
-      show_stateN(6);
-//      set_time();
-      Serial.println(modeIndex); //delete
+      digitalWrite(LED2, led_state);
+      digitalWrite(LED4, led_state);
+      show_time(alm_h1, alm_h0, alm_m1, alm_m0);
       break;
   }
   digitalWrite(BUZZ,HIGH);
@@ -317,16 +323,73 @@ void leds_off(){
 }
 
 void goto_next_mode() {
+  leds_off();
   modeIndex = modeIndex + 1;
   if ((modeIndex < 21) || (modeIndex > 27)) {
     modeIndex = 21;
   }
 }
 
-// TODO: Delete, this function was just to test if the states movement was right
-void show_stateN(int n) {
-  WriteNumberToSegment(0 , 0);
-  WriteNumberToSegment(1 , 0);
-  WriteNumberToSegment(2 , 0);
-  WriteNumberToSegment(3 , n);
+void update_clk_min() {
+  clk_m0 += 1;
+  if (clk_m0 >= 10) {
+    clk_m0 = 0;
+    clk_m1 += 1;
+  }
+  if (clk_m1 >= 6) {
+    clk_m1 = 0;
+    clk_h0 += 1;
+  }
+  if (clk_h1 == 2 && clk_h0 >= 4) {
+    clk_h0 = 0;
+    clk_h1 = 0;
+  }
+  else if (clk_h0 >= 10) {
+    clk_h0 = 0;
+    clk_h1 += 1;
+  }
+}
+
+void update_clk_hour() {
+  clk_h0 += 1;
+  if (clk_h1 == 2 && clk_h0 >= 4) {
+    clk_h0 = 0;
+    clk_h1 = 0;
+  }
+  else if (clk_h0 >= 10) {
+    clk_h0 = 0;
+    clk_h1 += 1;
+  }
+}
+
+void update_alm_min() {
+  alm_m0 += 1;
+  if (alm_m0 >= 10) {
+    alm_m0 = 0;
+    alm_m1 += 1;
+  }
+  if (alm_m1 >= 6) {
+    alm_m1 = 0;
+    alm_h0 += 1;
+  }
+  if (alm_h1 == 2 && alm_h0 >= 4) {
+    alm_h0 = 0;
+    alm_h1 = 0;
+  }
+  else if (alm_h0 >= 10) {
+    alm_h0 = 0;
+    alm_h1 += 1;
+  }
+}
+
+void update_alm_hour() {
+  alm_h0 += 1;
+  if (alm_h1 == 2 && alm_h0 >= 4) {
+    alm_h0 = 0;
+    alm_h1 = 0;
+  }
+  else if (alm_h0 >= 10) {
+    alm_h0 = 0;
+    alm_h1 += 1;
+  }
 }
