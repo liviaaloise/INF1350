24cce441efa086ac602522ccf183176a80c0703f
costalonga <33266172+costalonga@users.noreply.github.com>
2019-05-14 17:29:38 -0300

    Add files via upload
    Now using courotine for bullet

--- mini_projeto_LUA/main.lua
+++ mini_projeto_LUA/main.lua
@@ -39,7 +39,6 @@ local function newblip (vel, posx)
     end,
     
     getInactiveTime = function () return inactiveTime end
-    
   }
 end
 
@@ -86,56 +85,55 @@ end
 
 --                                                                               Bullet
 local function newbullet (player)
-  local sx = player.getX()
-  local sy = player.getY() - 1.5
+  local sx = player.getX() + 35/2
+  local sy = player.getY()
   local fire_status = false
   local bullet_wait = 0
   local width, height = love.graphics.getDimensions( )
   
   local wait = function (seg)
-    fire_status = false
     bullet_wait = love.timer.getTime() + seg
     coroutine.yield()
   end
   
   local function up()
---    while fire_status == true do
-      print("\n\tDENTRO DO WHILE\n")
-    if fire_status then  
-      sy = sy - 15.0 
+    while true do
+      if fire_status then
+        sy = sy - 3.0
+      end
       if sy < 0 then 
-        -- caso tiro passe da tela
         fire_status = false
       end
-      -- coroutine.yield()
---      wait(1.0)
+      wait(0.0001)
     end
   end
   return {
---    update = coroutine.wrap(up),
-    update = up,
+    update = coroutine.wrap(up),
+--    update = up,
     getSX = function () return sx end,
     getSY = function () return sy end,
     setSX = function (x) sx = x end,
+    setSY = function (y) sy = y end,
     setFireStatus = function (bool) fire_status = bool end,
     getFireStatus = function () return fire_status end,
     getWaitTime = function () return bullet_wait end,
     
     draw = function ()
       if fire_status == true then
-        love.graphics.polygon("fill", sx, sy, sx+3.5, sy, sx, sy-7.5)
+        love.graphics.polygon("fill", sx, sy, sx+3.5, sy, sx, sy-10.5)
       end
     end
   }
 end
 
 
---                                                  Keypressed
+--    Keypressed
 function love.keypressed(key)
   if key == 'a' then
     
     bullet.setFireStatus(true)
-    bullet.setSX(player.getX())
+    bullet.setSX(player.getX()+35/2)
+    bullet.setSY(player.getY())
     
     pos = player.try()
     for i in ipairs(listabls) do
@@ -148,8 +146,11 @@ function love.keypressed(key)
   end
 end
 
+
+--      LOAD
 function love.load()
   player =  newplayer()
+  bullets = {}
   bullet = newbullet(player)
   listabls = {}
   for i = 1, 5 do
@@ -157,6 +158,8 @@ function love.load()
   end
 end
 
+
+--      DRAW
 function love.draw()
   player.draw()
   for i = 1,#listabls do
@@ -165,26 +168,25 @@ function love.draw()
   bullet.draw()
 end
 
-function love.update(dt)
-  
-  nowTime = love.timer.getTime()
 
-  player.update(dt)
+--      LOAD
+function love.update(dt)
+  nowTime = love.timer.getTime()
+  
+  -- Update Player
+  player.update(dt) 
+  
+  -- Update blips
   for i = 1,#listabls do
     if listabls[i].getInactiveTime() <= nowTime then
       listabls[i].update()
     end
---    if listabls[i].getActivity() == true then
---      listabls[i].update()
---    end
---    print(i, listabls[i].getActivity(), listabls[i].getInactiveTime(), nowTime)
---    print(i, listabls[i].getInactiveTime(), nowTime)
---  print(player.getFireStatus())
   end
---  if bullet.getWaitTime() <= nowTime then
---    bullet.update()
---  end
-  bullet.update()
+  
+  -- Update Bullet
+  if bullet.getWaitTime() <= nowTime then
+    bullet.update()
+  end
   print("Sx: ", bullet.getSX(), "| Sy: ", bullet.getSY(), bullet.getFireStatus())
 end
   
